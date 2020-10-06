ed8e5a0cbad437d37df71748d9d0830e8de243ae
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2020-10-06 21:24:47 +0300

    Added Mysql to vagrant

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,4 +1,14 @@
+#variables
+DBHOST=localhost
+DBNAME=Pallas
+DBUSER=testi
+DBPASSWD=testpass
+
+
 # Update packages
+sudo apt-get update
+
+#Update ubunt
 sudo apt-get update
 
 # Install Node.js and NPM
@@ -8,7 +18,22 @@ sudo apt-get install -y nodejs
 # Install npm
 sudo npm install -g npm@6.14.6
 
-#Update ubunt
-sudo apt-get update
+#install mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+apt-get install -y mysql-server mysql-client
 
-#Tempporary commads here
+#Creat databases and user then grant privaleges
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASSWD'"
+mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'localhost'"
+mysql -uroot -p$DBPASSWD -e "CREATE USER '$DBUSER'@'10.0.2.2' IDENTIFIED BY '$DBPASSWD'"
+mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'10.0.2.2'"
+
+
+
+
+
+
+
+#Temppeorary commads here
