2d80de05d7c659b42c3443cce0a622376556ad6d
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2020-10-26 23:04:54 +0200

    Added segment get from database with points

--- src/routers/db.js
+++ src/routers/db.js
@@ -19,6 +19,37 @@ router.get('/points', function(req, res) {
   });
 });
 
-
+router.get('/segments', function(req, res) {
+  //get points from database
+  database.query('SELECT * FROM Koordinaatit ORDER BY Segmentti', function (err, points, fields) {
+      if (err) throw err;
+      //transfere needed data to array
+      const coordsForSegments = points.map((item) => {
+        item.Sijainti.lat = item.Sijainti.x;
+        item.Sijainti.lon = item.Sijainti.y;
+        delete item.Sijainti.x;
+        delete item.Sijainti.y;
+        return [item.Segmentti, item.Sijainti];
+      });
+      //get segments from database
+      database.query('SELECT * FROM Segmentit', function (err, result, fields) {
+        pointsDict = []
+        //create dictionary of arrays
+        result.forEach(obj =>{
+          pointsDict[obj.ID] = []
+        });
+        //Fill points to it
+        coordsForSegments.forEach(obj =>{
+          pointsDict[obj[0]].push(obj[1]);
+        });
+        //add array as object property to result
+        result.forEach(obj =>{
+          obj.Points = pointsDict[obj.ID];
+        });
+        
+        res.json(result);   
+      });
+  });
+});
 
 module.exports = router;
