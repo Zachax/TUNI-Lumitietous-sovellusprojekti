18b4a79989cb82a9f6da93d39d56ef104e14091f
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2020-10-28 15:11:03 +0200

    Added database data initalization to vagrant boot

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,9 +8,6 @@ DBPASSWD=testpass
 # Update packages
 sudo apt-get update
 
-#Update ubunt
-sudo apt-get update
-
 # Install Node.js and NPM
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -29,9 +26,9 @@ mysql -uroot -p$DBPASSWD -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$D
 mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'localhost'"
 mysql -uroot -p$DBPASSWD -e "CREATE USER '$DBUSER'@'10.0.2.2' IDENTIFIED BY '$DBPASSWD'"
 mysql -uroot -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'10.0.2.2'"
-
-
-
+mysql Pallas< ../../vagrant/src/sql/luonnit.sql -uroot -p$DBPASSWD
+mysql Pallas< ../../vagrant/src/sql/Segmentit.sql -uroot -p$DBPASSWD
+mysql Pallas< ../../vagrant/src/sql/Koordinaatit.sql -uroot -p$DBPASSWD
 
 
 
