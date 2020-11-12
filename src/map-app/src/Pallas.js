994e6380a4853e2d99d004c10e2c7d658a94d2a0
Markku Nirkkonen <markku.nirkkonen@tuni.fi>
2020-11-12 22:59:31 +0200

    Uusi paremmin toimiva kartta ja joitakin muita muutoksia

--- src/map-app/src/Pallas.js
+++ src/map-app/src/Pallas.js
@@ -2,31 +2,41 @@ import * as React from "react";
 import { useEffect } from 'react';
 import './App.css';
 import './style.css';
-import Map from './Map';
+import Map from './NewMap';
 import Info from './Info';
 import TopBar from './TopBar';
 import { useMediaQuery } from 'react-responsive';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar
+}));
 
 function App() {
 
   // Use state hooks
   const [token, setToken] = React.useState(null);
-  const [loaded, setLoaded] = React.useState(false);
+  //const [loaded, setLoaded] = React.useState(false);
   const [segments, setSegments] = React.useState([]);
+  const [updates, setUpdates] = React.useState([]);
   const [shownSegment, setShownSegment] = React.useState(null);
 
   //imported hooks
   const isMobile = useMediaQuery({query: '(max-width:760px)'});
 
+  const styledClasses = useStyles();
 
   // after rendering segment data is fetched - should this be in some other way?
   useEffect(() => {
     const fetchData = async () => {
+      const updates = await fetch('api/segments/update');
+      const updateData = await updates.json();
+      console.log(updateData);
+      setUpdates(updateData);
       const response = await fetch('api/segments');
       const data = await response.json();
       setSegments(data);
-      setLoaded(true);
+      //setLoaded(true);
     };
     fetchData();
   }, []);
@@ -46,19 +56,29 @@ function App() {
           <TopBar isMobile={isMobile} token={token} updateToken={updateToken} />   
         </div>
         <div id="map">
-          <Map 
-            segments={segments} 
-            onClick={chooseSegment} 
-            loaded={loaded} 
-            isMobile={isMobile} 
-          />
+          <div className={styledClasses.toolbar} />
+
+            <Map 
+              segments={segments} 
+              onClick={chooseSegment} 
+              //loaded={loaded} 
+              isMobile={isMobile} 
+            />
+
         </div>
         <div className="guide"></div>
         <div className="segment_info">
-          <Info 
-            segmentdata={segments[shownSegment-1]} 
-            token={token} 
-          />
+          <div className={styledClasses.toolbar} />
+          {(shownSegment !== null ? 
+            <Info
+              updates={updates}
+              segmentdata={shownSegment} 
+              token={token} 
+            />
+            :
+            <div />
+          )}
+          
         </div>
     </div>
   );
