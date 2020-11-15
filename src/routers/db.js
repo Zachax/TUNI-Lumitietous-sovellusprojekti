0631393cbe65da86993fa1a96b815f9c6e5b9ab8
Aarre <aarre.leinonen@tuni.fi>
2020-11-15 15:30:24 +0200

    Luotu APIin getti jolla saadaan lumilaadut

--- src/routers/db.js
+++ src/routers/db.js
@@ -168,6 +168,16 @@ router.get('/segments/update', function(req, res) {
   });
 });
 
+router.get('/lumilaadut', function(req, res) {
+    database.query('Select * FROM Lumilaadut', 
+    function(err, result, fields) {
+      if (err) throw err;
+      console.log(result)
+      res.json(result);
+      res.status(200);
+    });
+});
+
 router.use(function(req, res, next) {
 
   if (req.headers.authorization) {
