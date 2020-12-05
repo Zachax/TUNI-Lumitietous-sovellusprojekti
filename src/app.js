0ad58ea4421ca711f5639f15c6af5ecaeb74d741
Arttu-Lakkala <arttuje.lakkala@gmail.com>
2020-12-05 15:23:12 +0200

    Nimimuutosten bugien poisto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./routers/db');
+const api = require('./routers/api');
 const bodyParser = require('body-parser');
 const path = require('path');
 
