d7096beae751d9dbc5328e53a9d2fe44577f3a42
Aarre <aarre.leinonen@tuni.fi>
2021-01-29 12:17:07 +0200

    Siistitty läöhdekoodia, päivitetty dokumentteja

--- src/map-app/src/index.js
+++ src/map-app/src/index.js
@@ -1,3 +1,5 @@
+// React automatically created file
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
--- src/map-app/src/reportWebVitals.js
+++ src/map-app/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// React automatically created file
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
